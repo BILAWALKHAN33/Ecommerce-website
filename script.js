0f17c44e07e9a3344d65ec17ad06fc8c8f79ef41
Bilawal Khan <bilawalkhanrind36@gmail.com>
2024-08-26 04:06:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Optionally add a delay before the animation starts
+setTimeout(function() {
+    document.querySelector('.banner').style.animationPlayState = 'running';
+  }, 2000); // Adjust the delay in milliseconds (e.g., 2000ms is a 2 second delay)
+  
